2072c0914e0b2d57dd7946879a52662dd9eb65b0
SheikhULAB <tosibalaptop86@gmail.com>
2022-06-22 18:51:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import AddBook from './features/books/AddBook';
+import BooksView from './features/books/BooksView';
+import EditBook from './features/books/EditBook';
+import Error from './pages/Error';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar />
+    <main>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path='/show-books' element={<BooksView />} />
+         <Route path='/add-books' element={<AddBook />} />
+         <Route path='/edit-book' element={<EditBook />} />
+         <Route path='*' element={<Error />} />
+       </Routes>
+       </main> 
+       <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error Page || You entered wrong Url</h1>
+        </div>
+    );
+};
+
+export default Error;
